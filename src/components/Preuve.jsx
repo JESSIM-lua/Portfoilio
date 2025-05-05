c73f331155ec14031738a73bb4ed4af63c2f7493
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2025-05-05 23:21:49 +0200

    QA

--- src/components/Preuve.jsx
+++ src/components/Preuve.jsx
@@ -18,28 +18,28 @@ const Preuve = () => {
           period: "06/11/2024 - 03/12/2025",
           values: [true, false, false, false, false, false],
           valueLink:
-            "https://drive.google.com/file/d/1x676aGnDrDgLNojJd59trHYBcrEDvF_L/view",
+            "https://drive.google.com/file/d/1lSNkFPIj06V8-EUOkWifaxChf9G42pui/view?usp=sharing",
         },
         {
           name: "GLPI - TP de gestion d'incident où nous avons vu les différents niveaux dans la hiérarchie",
           period: "11/09/2024 - 02/10/2024",
           values: [false, true, false, false, false, false],
           valueLink:
-            "https://drive.google.com/file/d/1x676aGnDrDgLNojJd59trHYBcrEDvF_L/view",
+            "https://drive.google.com/file/d/1OzxTZGk3nvCfEASlllmqQcjMrcp9IiE-/view?usp=sharing",
         },
         {
           name: "Développement Présence en ligne - TP mené seul - Création d’un site web Wordpress, le but étant qu’il ait le meilleur référencement.",
           period: "17/01/2024 - 19/04/2025",
           values: [false, false, true, false, false, false],
           valueLink:
-            "https://drive.google.com/file/d/1x676aGnDrDgLNojJd59trHYBcrEDvF_L/view",
+            "https://drive.google.com/file/d/1PM6UnRm8_SiXMW5XL0cVOHti5zS8hOk1/view?usp=sharing",
         },
         {
           name: "Introspection - Nous devons nous décrire et décrire la voie que nous souhaitons prendre",
           period: "11/09/2023 - 19/04/2025",
           values: [false, false, false, false, false, true],
           valueLink:
-            "https://drive.google.com/file/d/1x676aGnDrDgLNojJd59trHYBcrEDvF_L/view",
+            "https://drive.google.com/file/d/1wen34OGuevf5ne3AUl-isC6RzE-yTjDb/view?usp=sharing",
         },
       ],
     },
@@ -64,7 +64,7 @@ const Preuve = () => {
         {
           name: "Création d’une application web (Walrus)",
           period: "03/03/2025 - 26/04/2027",
-          values: [false, false, true, false, false, false],
+          values: [false, false, false, true, false, false],
           valueLink:
             "https://drive.google.com/file/d/1x676aGnDrDgLNojJd59trHYBcrEDvF_L/view",
         },
